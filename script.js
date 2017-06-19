85a616cfede3210414ec4d9d1633d95db08d1be2
hakiemaul <noersedya@gmail.com>
2017-06-19 11:14:41 +0700

    goal scored data is displayed

--- script.js
+++ script.js
@@ -15,6 +15,29 @@ let svg = d3.select('#results')
 // Data reloading
 let reload = () => {
   // Your data parsing here...
+  let data = []
+  d3.tsv("afcw-results.tsv", function (d) {
+    return d
+  }, function (error, data) {
+    let goal = data.map((d) => {
+      return d.GoalsScored
+    })
+    svg.selectAll('rect')
+      .data(goal)
+      .enter()
+      .append('rect')
+      .attr('class', 'bar')
+      .attr('x', (d, i) => {
+        return i * 22
+      })
+      .attr('y', (d) => {
+        return height - (d * 100)
+      })
+      .attr('width', 20)
+      .attr('height', (d) => {
+        return d * 100
+      })
+  })
 }
 
 // redraw function
